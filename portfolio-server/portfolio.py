bf71c03750e46258119bc98e7e9731d3901268a4
Kaushik S Kalmady <19279785+kaushiksk@users.noreply.github.com>
2021-06-04 23:47:24 +0530

    Suppor to Add/Remove/Assign Goals (#11)
    * Add /goals/ route
    
    * Add explicit id for user info entry
    
    * Add goal and remove goal endpoints
    
    * Assign goal endpoint
    
    * Add better error reporting for remove goal

--- portfolio-server/portfolio.py
+++ portfolio-server/portfolio.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request
 from .db import get_db
 
 bp = Blueprint("portfolio", __name__, url_prefix="/portfolio")
@@ -8,6 +8,53 @@ bp = Blueprint("portfolio", __name__, url_prefix="/portfolio")
 def overview():
     _db = get_db()
     _schemes = _db.schemes.find(projection={"_id": False, "transactions": False})
-    user_info = _db.user_info.find_one({}, projection={"_id": False})
+    user_info = _db.user_info.find_one({})
     data = {"user_info": user_info, "schemes": [s for s in _schemes]}
     return data, 200
+
+
+@bp.route("/goals/", methods=["GET"])
+def goals():
+    _db = get_db()
+    user_goals = _db.user_info.find_one(projection={"goals": 1})
+    return user_goals, 200
+
+
+@bp.route("/goals/addgoal", methods=["POST"])
+def addgoal():
+    data = request.get_json()
+    goal = data.get("goal", None)
+
+    if goal is not None:
+        _db = get_db()
+        _db.user_info.update_one({"_id": 1}, {"$addToSet": {"goals": goal}})
+        return {"isSuccess": True}, 200
+
+    return {"error": "Json payload needs key 'goal'"}, 400
+
+
+@bp.route("/goals/removegoal", methods=["POST"])
+def removegoal():
+    data = request.get_json()
+    goal = data.get("goal", None)
+
+    if goal is not None:
+        _db = get_db()
+
+        is_valid_goal = _db.user_info.count_documents({"goals": goal})
+
+        if is_valid_goal:
+            schemes_with_goal = _db.schemes.count_documents({"goal": goal})
+
+            if schemes_with_goal == 0:
+                d = _db.user_info.update_one({"_id": 1}, {"$pull": {"goals": goal}})
+                print(d.modified_count)
+                return {"isSuccess": True}, 200
+
+            return {
+                "error": "Cannot remove goal. There are schemes which have this goal assigned."
+            }, 500
+
+        return {"error": "Cannot remove goal. Goal does not exist."}, 500
+
+    return {"error": "Json payload needs key 'goal'"}, 400
