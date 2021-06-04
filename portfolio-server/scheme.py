99c8b09396e85f8b38dabaeadb6d7367d8b64940
kaushiksk <19279785+kaushiksk@users.noreply.github.com>
2021-06-04 23:35:43 +0530

    Assign goal endpoint

--- portfolio-server/scheme.py
+++ portfolio-server/scheme.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request
 from .db import get_db
 
 bp = Blueprint("scheme", __name__, url_prefix="/scheme")
@@ -9,3 +9,25 @@ def scheme_details(amfi_id):
     _db = get_db()
     data = _db.schemes.find_one_or_404({"amfi": amfi_id}, projection={"_id": False})
     return data, 200
+
+
+@bp.route("/<amfi_id>/assigngoal", methods=["POST"])
+def assigngoal(amfi_id):
+    _db = get_db()
+    _db.schemes.find_one_or_404({"amfi": amfi_id})
+
+    data = request.get_json()
+    goal = data.get("goal", None)
+
+    if goal is not None:
+        is_valid_goal = _db.user_info.count_documents({"goals": goal})
+
+        if is_valid_goal:
+            _db.schemes.update_one({"amfi": amfi_id}, {"$set": {"goal": goal}})
+            return {"isSuccess": True}, 200
+
+        return {
+            "error": "Provided goal is not part of user's goals. Add it first."
+        }, 422
+
+    return {"error": "Json payload needs key 'goal'"}, 400
