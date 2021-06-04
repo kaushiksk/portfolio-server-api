754113589604f611e0926b596b691f2cfc6930b6
kaushiksk <19279785+kaushiksk@users.noreply.github.com>
2021-06-04 22:57:10 +0530

    Add goal and remove goal endpoints

--- portfolio-server/portfolio.py
+++ portfolio-server/portfolio.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request
 from .db import get_db
 
 bp = Blueprint("portfolio", __name__, url_prefix="/portfolio")
@@ -18,3 +18,37 @@ def goals():
     _db = get_db()
     user_goals = _db.user_info.find_one(projection={"goals": 1})
     return user_goals, 200
+
+
+@bp.route("/goals/addgoal", methods=["POST"])
+def addgoal():
+    data = request.get_json()
+    goal = data.get("goal", None)
+
+    if goal is not None:
+        _db = get_db()
+        _db.user_info.update_one({"_id": 1}, {"$addToSet": {"goals": goal}})
+        return {"isSuccess": True}, 200
+
+    return {"error": "Json payload needs key 'goal'"}, 400
+
+
+@bp.route("/goals/removegoal", methods=["POST"])
+def removegoal():
+    data = request.get_json()
+    goal = data.get("goal", None)
+
+    if goal is not None:
+        _db = get_db()
+
+        schemes_with_goal = _db.schemes.count_documents({"goal": goal})
+
+        if schemes_with_goal == 0:
+            _db.user_info.update_one({"_id": 1}, {"$pull": {"goals": goal}})
+            return {"isSuccess": True}, 200
+
+        return {
+            "error": "Cannot remove goal. There are schemes which have this goal assigned."
+        }, 500
+
+    return {"error": "Json payload needs key 'goal'"}, 400
