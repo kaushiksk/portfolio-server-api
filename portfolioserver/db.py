ee2a51611910baff2d35bdad3b488f3842bb1844
kaushiksk <19279785+kaushiksk@users.noreply.github.com>
2021-06-06 16:36:02 +0530

    Remove flask-pymongo dependency

--- portfolioserver/db.py
+++ portfolioserver/db.py
@@ -1,37 +1,23 @@
-from flask.cli import with_appcontext
-from flask_pymongo import PyMongo
-from flask import current_app
+from pymongo import MongoClient
 from pyportfolio import Portfolio
+from .configs import DevelopmentConfig
 from .codec_options import get_type_registry
 import click
 import cutie
 import os
 import sys
 
-CAS_FILE_PATH = "./cas-portfolio.pdf"
-mongo = PyMongo()
+config = DevelopmentConfig()
+mongo = MongoClient(config.MONGO_URI, type_registry=get_type_registry())
 
 
 def get_db():
-    return mongo.db
+    return mongo.get_default_database()
 
 
-def init_app(app):
-    mongo.init_app(app, type_registry=get_type_registry())
-    app.cli.add_command(init_db_command)
-
-
-@click.command("init-db")
-@with_appcontext
-def init_db_command():
-    if not os.path.exists(CAS_FILE_PATH):
-        print(
-            "File not found. Make sure you saved your CAS file as cas-portfolio.pdf in directory root"
-        )
-        sys.exit(0)
-
+def init_db_from_cas_file(cas_file_path):
     password = cutie.secure_input("Please enter the pdf password:")
-    p = Portfolio(CAS_FILE_PATH, password)
+    p = Portfolio(cas_file_path, password)
     data = p.to_json()
 
     db = get_db()
