f14a9f430ad98f71d6a2dde0ece0a9e82a690764
kaushiksk <19279785+kaushiksk@users.noreply.github.com>
2021-06-24 23:38:55 +0530

    Add pydantic models for schemes API

---
+++ portfolioserver/models.py
@@ -0,0 +1,56 @@
+from datetime import date
+from typing import Optional, List, Union
+from pydantic import BaseModel as PydanticBaseModel
+from decimal import Decimal
+import locale
+
+locale.setlocale(locale.LC_NUMERIC, "en_IN")
+
+class BaseModel(PydanticBaseModel):
+    class Config:
+        json_encoders = {
+            Decimal: lambda x: locale.format_string("%.4f", x, grouping=True)
+        }
+
+class GenericPostResponse(BaseModel):
+    isSuccess: Optional[bool] = False
+    error: Optional[str] = None
+
+class GoalRequest(BaseModel):
+    name: str
+
+class Transaction(BaseModel):
+    date: date
+    description: str
+    amount: Decimal
+    units: Union[Decimal, None]
+    nav: Union[Decimal, None]
+    balance: Union[Decimal, None]
+    type: str
+    dividend_rate: Union[Decimal, None]
+    Days: int
+
+class Scheme(BaseModel):
+    amfi: str
+    name: str
+    units: Decimal
+    nav: Decimal
+    valuation: Decimal
+    type: str
+    subtype: str
+    goal: str
+
+class SchemeWithTransactions(Scheme):
+    transactions: List[Transaction]
+
+class UserInfo(BaseModel):
+    name: str
+    email: str
+    address: str
+    mobile: str
+    valuation: Decimal
+    goals: List[str]
+
+class Portfolio(BaseModel):
+    user_info: UserInfo
+    schemes: List[Scheme]
