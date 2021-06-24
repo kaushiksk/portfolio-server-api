6c21eec837e44936f1e48f095f2dbeb0c39c5ccb
Kaushik S Kalmady <19279785+kaushiksk@users.noreply.github.com>
2021-06-24 23:54:53 +0530

    Add pydantic support for json validation and typing (#32)
    * Update codec to read as Decimal
    
    * Add pydantic models for schemes API
    
    * Use models in schemes api
    
    * Rename APIS
    
    - Move overview endpoint to portfolio
    - use /details for scheme details
    
    * Remove unnecessary imports
    
    * Use models in portfolio apis
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- portfolioserver/portfolio.py
+++ portfolioserver/portfolio.py
@@ -1,7 +1,18 @@
 from fastapi import APIRouter, Body, Depends
 from .db import get_db
+from .models import GoalRequest, Portfolio, GenericPostResponse
+from .errors import CANNOT_REMOVE_GOAL_EXISTS_IN_SCHEMA, CANNOT_REMOVE_GOAL_NOT_FOUND
 
 router = APIRouter(prefix="/portfolio", tags=["portfolio"])
+
+
+@router.get("/overview", response_model=Portfolio)
+def overview(_db=Depends(get_db)):
+    _schemes = [
+        s for s in _db.schemes.find(projection={"_id": False, "transactions": False})
+    ]
+    user_info = _db.user_info.find_one({}, projection={"_id": False})
+    return Portfolio(user_info=user_info, schemes=_schemes)
 
 
 @router.get("/goals/", status_code=200)
@@ -10,28 +21,23 @@ def goals(_db=Depends(get_db)):
     return user_goals
 
 
-@router.post("/goals/addgoal", status_code=201)
-def addgoal(goal: str = Body(...), _db=Depends(get_db)):
-    if goal is not None:
-        _db.user_info.update_one({"_id": 1}, {"$addToSet": {"goals": goal}})
-        return {"isSuccess": True}
+@router.post("/goals/addgoal", status_code=201, response_model=GenericPostResponse)
+def addgoal(goal: GoalRequest, _db=Depends(get_db)):
+    _db.user_info.update_one({"_id": 1}, {"$addToSet": {"goals": goal.name}})
+    return GenericPostResponse(isSuccess=True)
 
 
-@router.post("/goals/removegoal")
-def removegoal(goal: str = Body(...), _db=Depends(get_db)):
-    if goal is not None:
-        is_valid_goal = _db.user_info.count_documents({"goals": goal})
+@router.post("/goals/removegoal", response_model=GenericPostResponse)
+def removegoal(goal: GoalRequest, _db=Depends(get_db)):
+    is_valid_goal = _db.user_info.count_documents({"goals": goal.name})
 
-        if is_valid_goal:
-            schemes_with_goal = _db.schemes.count_documents({"goal": goal})
+    if is_valid_goal:
+        schemes_with_goal = _db.schemes.count_documents({"goal": goal.name})
 
-            if schemes_with_goal == 0:
-                d = _db.user_info.update_one({"_id": 1}, {"$pull": {"goals": goal}})
-                print(d.modified_count)
-                return {"isSuccess": True}
+        if schemes_with_goal == 0:
+            _db.user_info.update_one({"_id": 1}, {"$pull": {"goals": goal.name}})
+            return GenericPostResponse(isSuccess=True)
 
-            return {
-                "error": "Cannot remove goal. There are schemes which have this goal assigned."
-            }
+        return GenericPostResponse(error=CANNOT_REMOVE_GOAL_EXISTS_IN_SCHEMA)
 
-        return {"error": "Cannot remove goal. Goal does not exist."}
+    return GenericPostResponse(error=CANNOT_REMOVE_GOAL_NOT_FOUND)
