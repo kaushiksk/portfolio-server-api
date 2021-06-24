891251488b16c9e12a3cb1cd43c13996bf28d780
kaushiksk <19279785+kaushiksk@users.noreply.github.com>
2021-06-24 23:48:22 +0530

    Use models in portfolio apis

--- portfolioserver/portfolio.py
+++ portfolioserver/portfolio.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Body, Depends
 from .db import get_db
-from .models import Portfolio
+from .models import GoalRequest, Portfolio, GenericPostResponse
+from .errors import CANNOT_REMOVE_GOAL_EXISTS_IN_SCHEMA, CANNOT_REMOVE_GOAL_NOT_FOUND
 
 router = APIRouter(prefix="/portfolio", tags=["portfolio"])
 
@@ -17,28 +18,23 @@ def goals(_db=Depends(get_db)):
     return user_goals
 
 
-@router.post("/goals/addgoal", status_code=201)
-def addgoal(goal: str = Body(...), _db=Depends(get_db)):
-    if goal is not None:
-        _db.user_info.update_one({"_id": 1}, {"$addToSet": {"goals": goal}})
-        return {"isSuccess": True}
+@router.post("/goals/addgoal", status_code=201, response_model=GenericPostResponse)
+def addgoal(goal: GoalRequest, _db=Depends(get_db)):
+    _db.user_info.update_one({"_id": 1}, {"$addToSet": {"goals": goal.name}})
+    return GenericPostResponse(isSuccess=True)
 
 
-@router.post("/goals/removegoal")
-def removegoal(goal: str = Body(...), _db=Depends(get_db)):
-    if goal is not None:
-        is_valid_goal = _db.user_info.count_documents({"goals": goal})
+@router.post("/goals/removegoal", response_model=GenericPostResponse)
+def removegoal(goal: GoalRequest, _db=Depends(get_db)):
+    is_valid_goal = _db.user_info.count_documents({"goals": goal.name})
 
-        if is_valid_goal:
-            schemes_with_goal = _db.schemes.count_documents({"goal": goal})
+    if is_valid_goal:
+        schemes_with_goal = _db.schemes.count_documents({"goal": goal.name})
 
-            if schemes_with_goal == 0:
-                d = _db.user_info.update_one({"_id": 1}, {"$pull": {"goals": goal}})
-                print(d.modified_count)
-                return {"isSuccess": True}
+        if schemes_with_goal == 0:
+            _db.user_info.update_one({"_id": 1}, {"$pull": {"goals": goal.name}})
+            return GenericPostResponse(isSuccess=True)
 
-            return {
-                "error": "Cannot remove goal. There are schemes which have this goal assigned."
-            }
+        return GenericPostResponse(error=CANNOT_REMOVE_GOAL_EXISTS_IN_SCHEMA)
 
-        return {"error": "Cannot remove goal. Goal does not exist."}
+    return GenericPostResponse(error=CANNOT_REMOVE_GOAL_NOT_FOUND)
