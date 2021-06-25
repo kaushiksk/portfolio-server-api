9ed2ce0b094dcd6e2c06f59c6d2f2937d6ad23dc
Kaushik S Kalmady <19279785+kaushiksk@users.noreply.github.com>
2021-06-25 16:27:33 +0530

    Use GoalsExport type for export/import (#40)

--- portfolioserver/goals.py
+++ portfolioserver/goals.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import FileResponse
 from .db import get_db
-from .models import GoalRequest, GenericPostResponse, GoalResponse
+from .models import GoalRequest, GenericPostResponse, GoalResponse, GoalsExport
 from .errors import CANNOT_REMOVE_GOAL_EXISTS_IN_SCHEMA, CANNOT_REMOVE_GOAL_NOT_FOUND
 
 router = APIRouter(prefix="/goals", tags=["goals"])
@@ -35,23 +35,20 @@ def remove_goal(goal: GoalRequest, _db=Depends(get_db)):
     return GenericPostResponse(error=CANNOT_REMOVE_GOAL_NOT_FOUND)
 
 
-@router.get("/export")
+@router.get("/export", response_model=GoalsExport)
 def export_goals(_db=Depends(get_db)):
     user_goals = _db.user_info.find_one(projection={"_id": False, "goals": True})
     scheme_goals = _db.schemes.find(
         {}, projection={"_id": False, "amfi": True, "goal": True}
     )
 
-    output_data = {}
-    output_data["goals"] = user_goals["goals"]
-    output_data["schemes"] = [s for s in scheme_goals]
+    schemes = [s for s in scheme_goals]
+    output_data = GoalsExport(goals=user_goals["goals"], schemes=schemes)
 
-    import json
     import tempfile
 
-    output_json = json.dumps(output_data, indent=4)
     with tempfile.NamedTemporaryFile("w", delete=False) as file:
-        file.write(output_json)
+        file.write(output_data.json(indent=4))
         return FileResponse(
             file.name, media_type="application/json", filename="goals.json"
         )
