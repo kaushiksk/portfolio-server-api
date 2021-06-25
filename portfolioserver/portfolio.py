c962ae9f66ca9199a5ab9ca189af6eac5a2dc5d7
kaushiksk <19279785+kaushiksk@users.noreply.github.com>
2021-06-25 14:40:51 +0530

    Adds export goals endpoint

--- portfolioserver/portfolio.py
+++ portfolioserver/portfolio.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Body, Depends
+from fastapi import APIRouter, Depends
+from fastapi.responses import FileResponse
 from .db import get_db
 from .models import GoalRequest, Portfolio, GenericPostResponse, GoalResponse
 from .errors import CANNOT_REMOVE_GOAL_EXISTS_IN_SCHEMA, CANNOT_REMOVE_GOAL_NOT_FOUND
@@ -41,3 +42,27 @@ def remove_goal(goal: GoalRequest, _db=Depends(get_db)):
         return GenericPostResponse(error=CANNOT_REMOVE_GOAL_EXISTS_IN_SCHEMA)
 
     return GenericPostResponse(error=CANNOT_REMOVE_GOAL_NOT_FOUND)
+
+
+@router.get("/goals/export")
+def export_goals(_db=Depends(get_db)):
+    user_goals = _db.user_info.find_one(projection={"_id": False, "goals": True})
+    scheme_goals = _db.schemes.find(
+        {}, projection={"_id": False, "amfi": True, "goal": True}
+    )
+
+    output_data = {}
+    output_data["goals"] = user_goals["goals"]
+    output_data["schemes"] = [s for s in scheme_goals]
+
+    import json
+
+    output_json = json.dumps(output_data, indent=4)
+
+    import tempfile
+
+    with tempfile.NamedTemporaryFile("w", delete=False) as file:
+        file.write(output_json)
+        return FileResponse(
+            file.name, media_type="application/json", filename="goals.json"
+        )
