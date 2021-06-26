c59f09b5a562127d82be305cf54bfcfc5d93c205
kaushiksk <19279785+kaushiksk@users.noreply.github.com>
2021-06-26 19:42:57 +0530

    Move all router files to routes

--- portfolioserver/app.py
+++ portfolioserver/app.py
@@ -4,9 +4,7 @@ from fastapi import FastAPI
 def create_app():
     app = FastAPI()
 
-    from . import portfolio
-    from . import schemes
-    from . import goals
+    from .routes import portfolio, schemes, goals
 
     app.include_router(portfolio.router)
     app.include_router(schemes.router)
