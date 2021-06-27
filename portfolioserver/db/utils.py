06ef97cd72561eaadd4f5044ee22d0e0cf498d46
kaushiksk <19279785+kaushiksk@users.noreply.github.com>
2021-06-27 10:49:47 +0530

    use DEFAULT_PROJECTION

--- portfolioserver/db/utils.py
+++ portfolioserver/db/utils.py
@@ -1,23 +1,26 @@
 from .core import Database
 
 
+DEFAULT_PROJECTION = {"_id": False}
+
+
 def get_user_info(db: Database):
-    user_info = db.user_info.find_one({}, projection={"_id": False})
+    user_info = db.user_info.find_one({}, DEFAULT_PROJECTION)
     return user_info
 
 
 def get_all_schemes(db: Database):
-    schemes = db.schemes.find({}, projection={"_id": False})
-    return [s for s in schemes]
+    schemes = db.schemes.find({}, DEFAULT_PROJECTION)
+    return list(schemes)
 
 
 def get_scheme(db: Database, amfi: str):
-    data = db.schemes.find_one({"amfi": amfi}, projection={"_id": False})
+    data = db.schemes.find_one({"amfi": amfi}, DEFAULT_PROJECTION)
     return data
 
 
 def get_user_goals(db: Database):
-    return db.user_info.find_one(projection={"_id": False, "goals": True})
+    return db.user_info.find_one(projection={**DEFAULT_PROJECTION, "goals": True})
 
 
 def is_valid_goal(db: Database, goal: str):
